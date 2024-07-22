e75ff136ccbab4b1d87df933d70502068a22763e
YSH-NOTEBOOK\seung <hilov@naver.com>
2024-07-22 23:15:12 +0900

    주석 추가

--- Angularjs/app/src/app/servers/servers.component.ts
+++ Angularjs/app/src/app/servers/servers.component.ts
@@ -1,16 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
-  //selector: '[app-servers]',
-  //selector: '.app-servers',
+  selector: 'app-servers', // 표준?
+  //selector: '[app-servers]', //attr 속성
+  //selector: '.app-servers', // css class 속성
   /*
   template: `
     app server
     <app-server></app-server>
     <app-server></app-server>
   `,
-  */
+  */ // template url 대신 간단한 경우 사용 편의성 있음
   templateUrl: './servers.component.html',
   styleUrl: './servers.component.css'
 })
@@ -30,9 +30,11 @@ export class ServersComponent {
 
   onCreateServer() {
     this.serverCreationStatus = 'Server Was Created!';
+    console.log("onCreateServer this.serverCreationStatus => ", this.serverCreationStatus);
   }
   onUpdateServerName(event: Event) {
     console.log("event => ", event);
     this.serverName = (<HTMLInputElement>event.target).value;
+    console.log("this.serverName => ", this.serverName);
   }
 }
