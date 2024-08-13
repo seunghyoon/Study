b6a8b0a035ff2b9b908df009ced5c6677e84f365
YSH-NOTEBOOK\seung <hilov@naver.com>
2024-08-14 06:39:08 +0900

    앵귤러 dom 엘리먼트를  type script 찾는 방법 @ViewChild

--- Angularjs/databinding-app/src/app/cockpit/cockpit.component.ts
+++ Angularjs/databinding-app/src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
@@ -9,10 +9,11 @@ export class CockpitComponent implements OnInit{
   
   @Output('svCreated') serverCreated = new EventEmitter<{serverName: string, serverContent: string}>;
   @Output('bpCreated') bluePrintCreated = new EventEmitter<{serverName: string, serverContent: string}>;
-  
+  @ViewChild('serverContentInput') serverContentInput: ElementRef | any;
   //newServerName = '';
-  newServerContent = '';
+  //newServerContent = '';
   
+
   constructor () {}
 
   ngOnInit() {
@@ -21,12 +22,13 @@ export class CockpitComponent implements OnInit{
 
   
 
-  onAddServer(serverNameInput: any) {
-    console.log("serverNameInput ", serverNameInput, serverNameInput.value);
+  onAddServer(nameInput: HTMLInputElement) {
+    console.log("serverNameInput ", this.serverContentInput?.nativeElement.value);
     this.serverCreated.emit({
       //serverName: this.newServerName,
-      serverName: serverNameInput.value,
-      serverContent: this.newServerContent
+      serverName: nameInput.value,
+      //serverContent: this.newServerContent
+      serverContent: this.serverContentInput.nativeElement.value
     });
     /*
     this.serverElements.push({
@@ -37,13 +39,14 @@ export class CockpitComponent implements OnInit{
     */
   }
 
-  onAddBlueprint(serverNameInput: any) {
-    console.log("serverNameInput ", serverNameInput, serverNameInput.value);
-
+  onAddBlueprint(nameInput: HTMLInputElement) {
+    console.log("serverNameInput ", nameInput, nameInput.value);
+    this.serverContentInput.nativeElement.value ="asdfasdfasdfasdfasdf";
     this.bluePrintCreated.emit({
       //serverName: this.newServerName,
-      serverName: serverNameInput.value,
-      serverContent: this.newServerContent
+      serverName: nameInput.value,
+      //serverContent: this.newServerContent
+      serverContent: this.serverContentInput.nativeElement.value
     });
     /*
     this.serverElements.push({
