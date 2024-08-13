ea07a876e0ae71021b20e20bc983faae0811bed3
YSH-NOTEBOOK\seung <hilov@naver.com>
2024-08-14 06:06:47 +0900

    css

--- Angularjs/databinding-app/src/app/server-element/server-element.component.ts
+++ Angularjs/databinding-app/src/app/server-element/server-element.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
 import { ServerElement } from '../models/server-element.model';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
-  styleUrl: './server-element.component.css'
+  styleUrl: './server-element.component.css',
+  encapsulation: ViewEncapsulation.Emulated //None, Native default
 })
 export class ServerElementComponent implements OnInit{
   @Input('srvElement') element: ServerElement = {  type: '', name: '', content: ''};
